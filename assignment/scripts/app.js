288f3fa92061b12e54e39e230a2f09a67dc1a56b
Heidi Ganac Nicholson <heidiganac@gmail.com>
2023-01-20 17:38:17 -0600

    set up skeleton of assignment

---
+++ assignment/scripts/app.js
@@ -0,0 +1,15 @@
+$(document).ready(onReady);
+console.log();
+
+// STATE //
+
+
+// onReady Function //
+function onReady() {
+    console.log('ready now',);
+
+    render();
+
+    // Handel events here
+    
+}
